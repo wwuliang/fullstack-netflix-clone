b832e89d17e47cc25adae28b6598f98d3561d02d
wwuliang <williamwuliang22@gmail.com>
2023-12-24 19:16:50 -0500

    Authentication of Login and Registration credentials is completed 1:20:40

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { useCallback, useState } from 'react';
-import Input from "@/components/input"
+import Input from "@/components/input";
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const Auth = () => {
+    const router = useRouter();
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -13,6 +16,21 @@ const Auth = () => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, []);
 
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+
+            router.push('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, password, router]);
+
     const register = useCallback(async () => {
         try {
             await axios.post('/api/register', {
@@ -20,10 +38,12 @@ const Auth = () => {
                 name,
                 password,
             });
+
+            login();
         } catch (error) {
             console.log(error);
         }
-    }, [email, name, password]);
+    }, [email, name, password, login]);
 
     return (
         /* Background Image */
@@ -63,7 +83,7 @@ const Auth = () => {
                                 type="password"
                             />
                         </div>
-                        <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             Login
                         </button>
                         <p className="text-neutral-500 mt-12">
