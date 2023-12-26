2d158b1e1be5f134dd92761c182aba64d7849a9d
wwuliang <williamwuliang22@gmail.com>
2023-12-26 16:51:01 -0500

    Added NavBar Transparency Effect

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,36 @@ import { RiArrowDownSFill } from "react-icons/ri";
 import { IoSearch } from "react-icons/io5";
 import { FaRegBell } from "react-icons/fa";
 
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import NavbarItem from "@/components/NavbarItem";
 import MobileMenu from "@/components/MobileMenu";
 import AccountMenu from '@/components/AccountMenu';
 
+// Scrolling Transparency Effect on Navigation Bar
+const TOP_OFFSET = 66;
+
+// Navigation Bar
 const Navbar = () => {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
     const [showAccountMenu, setShowAccountMenu] = useState(false);
+    const [showBackground, setShowBackground] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= TOP_OFFSET) {
+                setShowBackground(true);
+            } 
+            else {
+                setShowBackground(false);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
 
     const toggleMobileMenu = useCallback(() => {
         setShowMobileMenu(prevShowMobileMenu => !prevShowMobileMenu);
@@ -21,7 +43,7 @@ const Navbar = () => {
 
     return (
         <nav className="w-full fixed z-40">
-            <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
+            <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''} `}>
                 <img className="h-4 lg:h-7" src="/images/logo.png" alt="Netflix Logo" />
                 {/* Navigation for Streaming */}
                 <div className="flex-row ml-8 gap-7 hidden lg:flex">
