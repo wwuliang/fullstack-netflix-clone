a10e6afafb6548a36c7e0dc3fb4579c9f0cfe964
wwuliang <williamwuliang22@gmail.com>
2023-12-26 15:36:58 -0500

    NavbarItem Componenet Added 2:05:58

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,18 @@
+import NavbarItem from "./NavbarItem";
+
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="w-full fixed z-40">
+            <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
+                <img className="h-4 lg:h-7" src="/images/logo.png" alt="Netflix Logo" />
+                <div className="flex-row ml-8 gap-7 hidden lg-flex">
+                    <NavbarItem label="Home"/>
+                    <NavbarItem label="Home"/>
+                    <NavbarItem label="Home"/>
+                    <NavbarItem label="Home"/>
+                </div>
+            </div>
+        </nav>
     )
 }
 
