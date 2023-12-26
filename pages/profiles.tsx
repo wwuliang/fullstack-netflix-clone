2c5d1ac320eade1c53a844f9e09a3809be37769d
wwuliang <williamwuliang22@gmail.com>
2023-12-26 14:30:18 -0500

    Began adding profiles page 1:52:41

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -2,7 +2,7 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
-    const session = await getSession();
+    const session = await getSession(context);
 
     if (!session) {
         return {
