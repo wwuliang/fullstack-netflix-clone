68e95e520bf58114aac7313937df898b9146219c
wwuliang <williamwuliang22@gmail.com>
2023-12-27 12:31:24 -0500

    Completed Billboard Component and Random Movie Selection (Random Movie does not work properly) 2:46:15

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,6 +25,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   )
 }
