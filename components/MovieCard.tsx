b10401fb6706a27dcd7774ff88c5a1edccb6606b
wwuliang <williamwuliang22@gmail.com>
2023-12-28 12:30:27 -0500

    MovieCard hover effect 3:00:56

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -9,7 +9,49 @@ const MovieCard: React.FC<MovieCardProps> = ({
 }) => {
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
-            <img src="{data.thumbnailUrl}" alt="Thumbnail" />
+            <img className="
+                    cursor-pointer 
+                    object-cover 
+                    transition 
+                    duration 
+                    shadow-xl 
+                    rounded-md 
+                    group-hover:opacity-90 
+                    sm:group-hover:opacity-0 
+                    delay-300 
+                    w-full 
+                    h-[12vw]
+                " 
+            src={data.thumbnailUrl} alt="Thumbnail" />
+            <div className="
+                opacity-0 
+                absolute 
+                top-0 
+                transition 
+                duration-200 
+                z-10 
+                invisible 
+                sm:visible 
+                delay-300 
+                w-full 
+                scale-0 
+                group-hover:scale-110 
+                group-hover:-translate-y-[6vw] 
+                group-hover:translate-x-[2vw]
+                group-hover:opacity-100
+            ">
+                <img className="
+                    cursor-pointer
+                    object-cover
+                    transition
+                    duration
+                    shadow-xl 
+                    rounded-md
+                    w-full
+                    h-[12vw]
+                "
+                src={data.thumbnailUrl} alt="Thumbnail" />
+            </div>
         </div>
     )
 }
