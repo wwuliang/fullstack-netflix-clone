9a80e27272f0374df106f3ea4b0589d9a8806bec
wwuliang <williamwuliang22@gmail.com>
2023-12-28 12:15:52 -0500

    Began the implementation of Movie List Columns 2:55:09

---
+++ components/MovieList.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { isEmpty } from 'lodash';
+
+interface MovieListProps {
+    data: Record<string, any>[];
+    title: string;
+}
+
+const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
+    if (isEmpty(data)) {
+        return null;
+    }
+
+    return (
+        <div className="px-4 md:px-12 mt-4 space-y-8">
+            <div className="">
+                <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
+                    {title}
+                </p>
+                <div className="grid grid-cols-4 gap-2">
+                    {data.map((movie) => (
+                        <div key={movie.id}>movie</div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MovieList;
---
+++ pages/api/movies/index.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prismadb from '@/lib/prismadb';
+import serverAuth from '@/lib/serverAuth';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        if (req.method !== 'GET') {
+        return res.status(405).end();
+        }
+  
+        await serverAuth(req, res);
+        const movies = await prismadb.movie.findMany();
+        return res.status(200).json(movies);
+
+    } catch (error) {
+        console.log({ error })
+        return res.status(500).end();
+    }
+  }
