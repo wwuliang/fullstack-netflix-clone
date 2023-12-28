a2d5967389791179b700196cad0b5ed158d531a7
wwuliang <williamwuliang22@gmail.com>
2023-12-28 12:15:43 -0500

    Began the implementation of Movie List Columns 2:55:09

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,10 +24,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
-      <Navbar />
-      <Billboard />
+      <Navbar/>
+      <Billboard/>
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies}/>
+      </div>
     </>
   )
 }
