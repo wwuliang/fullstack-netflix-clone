6ad47453fc755a8036d4f6dd86f5e616618a017d
wwuliang <williamwuliang22@gmail.com>
2023-12-29 02:01:37 -0500

    Ready for Deployment

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,8 +4,7 @@ import { NextPageContext } from 'next';
 import { getSession, signIn } from 'next-auth/react';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
-
-import Input from "@/components/Input";
+import Input from '@/components/Input';
 
 const Auth = () => {
     const [email, setEmail] = useState('');
